d00c5721af4952a95976ddf8e8d6cc1f64380502
Lucas Lima Cortez <lucascortez.llca@gmail.com>
2024-09-12 09:23:57 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-def contato(request):
-    return HttpResponse('contato')
+    return render(request, 'recipes/home.html', 
+                  context={'name': 'Lucas Cortez'})
